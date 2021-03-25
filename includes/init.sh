be35316c14380919996ee11a3a3ebc7924774dc7
KEVIN PEREZ <perez40390@hotmail.fr>
2021-03-25 10:31:39 +0100

    fix file
    on crée le fichier avant pour éviter des erreurs

--- includes/init.sh
+++ includes/init.sh
@@ -77,6 +77,7 @@ function createAdmin {
 function generateKeysForSFTP {
   echo "Lancement de la génération des clés SFTP, laissez vide les champs..."
   mkdir -p sftp && cd sftp || exit 1
+  touch users.conf
   ssh-keygen -t ed25519 -f ssh_host_ed25519_key < /dev/null
   ssh-keygen -t rsa -b 4096 -f ssh_host_rsa_key < /dev/null
   cd ..
